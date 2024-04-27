2604c810136b8ae4095eabc7a5b34c4f5c4b23bc
MrSharma <mrsharma@local.com>
2024-04-27 16:58:44 +0530

    gateway added

---
+++ src/main/java/com/example/lmsgatewayserver/LmsGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lmsgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsGatewayServerApplication.class, args);
+	}
+
+}
