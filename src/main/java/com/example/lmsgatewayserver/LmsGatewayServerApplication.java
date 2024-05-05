78ac4f9a4b5125c504c71fc88ce2c446609ecaf3
Manthan Sharma <67254655+MrSharma619@users.noreply.github.com>
2024-05-05 15:46:49 +0530

    info regarding firewall issue

--- src/main/java/com/example/lmsgatewayserver/LmsGatewayServerApplication.java
+++ src/main/java/com/example/lmsgatewayserver/LmsGatewayServerApplication.java
@@ -3,6 +3,7 @@ package com.example.lmsgatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//disable firewall or it might not work
 @SpringBootApplication
 public class LmsGatewayServerApplication {
 
